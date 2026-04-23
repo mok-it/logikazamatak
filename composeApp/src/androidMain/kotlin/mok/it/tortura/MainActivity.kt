bb02a646196286ba71661810ef1dd61dcd7a4751
Olivér Raisz <raiszoliver@gmail.com>
2026-04-23 14:48:31 +0200

    feat: Add Android deep link handling for Supabase Auth

--- composeApp/src/androidMain/kotlin/mok/it/tortura/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/mok/it/tortura/MainActivity.kt
@@ -1,19 +1,21 @@
 package mok.it.tortura
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import ca.gosyer.appdirs.impl.attachAppDirs
+import io.github.jan.supabase.auth.handleDeeplinks
 import io.github.vinceglb.filekit.FileKit
 import io.github.vinceglb.filekit.dialogs.init
-import java.io.File
-import mok.it.tortura.util.appDirs
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        SupabaseClient.client.handleDeeplinks(intent)
 
         FileKit.init(this)
 
@@ -22,6 +24,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             App()
         }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        SupabaseClient.client.handleDeeplinks(intent)
     }
 }
 
