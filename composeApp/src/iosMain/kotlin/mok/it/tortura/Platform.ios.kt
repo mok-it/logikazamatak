cab97cab02dca20b7d1d47c692853757a5c2cd53
Olivér Raisz <raiszoliver@gmail.com>
2026-04-23 13:17:33 +0200

    chore: removed platform.ios.kt

--- composeApp/src/iosMain/kotlin/mok/it/tortura/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/mok/it/tortura/Platform.ios.kt
@@ -1,14 +1,5 @@
 package mok.it.tortura
 
-import platform.UIKit.UIDevice
-import kotlin.system.exitProcess
-
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-}
-
-actual fun getPlatform(): Platform = IOSPlatform()
-
-actual fun goodNightGoodBye() {
-    exitProcess( 0 )
+actual fun getPlatform(): Platform {
+    TODO("Not yet implemented")
 }
